e38baaa85f6168860e7e189bc70714b3bc3d19bd
Hudson da Silva Carlos <hudson.carlos@localiza.com>
2022-01-10 21:53:01 -0300

    Feature - Adicionano projeto de treinamento sobre atributo do tipo volatile e Interlocked.

---
+++ InterlockedExemple/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace InterlockedExemple
+{
+    class InterlockedClass
+    {
+        //0 para falso, 1 para verdadeiro.
+        private static int _usandoRecurso = 0;
+        private const int _numeroInteracoesThread = 5;
+        private const int _numeroThreads = 10;
+
+        static void Main()
+        {
+            Thread minhaThread;
+            var randomico = new Random();
+
+            for (int i = 0; i < _numeroThreads; i++) 
+            {
+                minhaThread = new Thread(new ThreadStart(ProcessandoMinhaThread))
+                {
+                    Name = $"Thread{i + 1}"
+                };
+
+                // Espera um tempo aleatório antes de iniciar próxima thread.
+                Thread.Sleep(randomico.Next(0, 1000));
+                minhaThread.Start();
+            }
+
+            Console.ReadKey();
+        }
+
+        static void ProcessandoMinhaThread() 
+        {
+            for (int i = 0; i < _numeroInteracoesThread; i++)
+            {
+                UsoRecurso();
+
+                // Espera 1 segundo antes da procima tentativa.
+                Thread.Sleep(5000);
+            }
+        }
+
+        // Um simples método que nega reentrada.
+        static bool UsoRecurso()
+        {
+            // 0 Indica que o método não é utilizado.
+            if (0 == Interlocked.Exchange(ref _usandoRecurso, 1))
+            {
+                Console.WriteLine($"{Thread.CurrentThread.Name} - Bloqueado.");
+
+                // Código para acessar recurso que não é Thread safe iria aqui.
+
+                // Simula algum trabalho
+                Thread.Sleep(500);
+
+                Console.WriteLine($"{Thread.CurrentThread.Name} - Ainda existe um bloqueio.");
+
+                // Solta o bloqueio.
+                Interlocked.Exchange(ref _usandoRecurso, 0);
+                Console.WriteLine($"{Thread.CurrentThread.Name} - Desbloqueado.");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"{Thread.CurrentThread.Name} foi negado o bloqueio.");
+                return false;
+            }
+        }
+    }
+}
---
+++ VolatileExemple/Worker.cs
@@ -0,0 +1,25 @@
+﻿namespace VolatileExemple
+{
+    public class Worker
+    {
+        // Este método é chamado quando o thread é iniciado.
+        public void DoWork()
+        {
+            bool work = false;
+            while (!_shouldStopVolatile)
+            {
+                Thread.Sleep(5000);
+                work = !work; // simular algum trabalho
+            }
+            Console.WriteLine("3 - Thread de trabalho: terminando normalmente.");
+        }
+        public void RequestStop()
+        {
+            Console.WriteLine("2 - Propriedade _shouldStopVolatile sendo acessada por outra thread.");
+            _shouldStopVolatile = true;
+        }
+        // A palavra-chave volátil é usada como uma dica para o compilador de que esses dados
+        // membro é acessado por vários threads.
+        private volatile bool _shouldStopVolatile;
+    }
+}
