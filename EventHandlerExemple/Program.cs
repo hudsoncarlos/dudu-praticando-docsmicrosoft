4b06be6b5b8e14c6c00ee42cf1e571e59078036a
Hudson da Silva Carlos <hudson.carlos@localiza.com>
2022-01-15 19:46:23 -0300

    Feature - Novas features.

--- EventHandlerExemple/Program.cs
+++ EventHandlerExemple/Program.cs
@@ -1,10 +1,47 @@
-﻿namespace EventHandlerExemple
+﻿using System;
+
+namespace EventHandlerExemple
 {
     public class Program
     {
-        public static void Main()
+        static void Main()
         {
-            
+            var limite = new Random().Next(10);
+            Console.WriteLine($"Numero limite de adições = {limite}");
+
+            var contador = new Counter(limite);
+            contador.LimitAlcance += ContadorLimitAlcance;
+
+            Console.WriteLine("Digite a tecla 'a' incrementar o total.");
+            while (Console.ReadKey(true).KeyChar == 'a')
+            {
+                Console.WriteLine("Adicionando um.");
+                contador.Add(1);
+            }
+        }
+
+        static void ContadorLimitAlcance(object? sender, EventArgs e)
+        {
+            Console.WriteLine("O limite foi alcançado.");
+            Environment.Exit(0);
+        }
+
+        class Counter        
+        {
+            private int _limite;
+            private int _total;
+
+            public Counter(int passarLimite)
+               => _limite = passarLimite;
+
+            public void Add(int passarTotal)
+            {
+                _total += passarTotal;
+                if (_total >= _limite)
+                    LimitAlcance?.Invoke(this, EventArgs.Empty);
+            }
+
+            public event EventHandler LimitAlcance;
         }
     }
 }
